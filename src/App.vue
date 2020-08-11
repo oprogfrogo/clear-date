4ce1105850aabd910ac4a46ab83dd36541e5cb51
Simon Lee <slee@bslogin.com>
2020-08-11 11:13:15 -0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,38 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <component :is="layout">
+      <router-view />
+    </component>
   </div>
 </template>
 
-<style>
+<script>
+import Header from "./components/TopHeader";
+const default_layout = "default";
+
+export default {
+  components: {
+    "top-header": Header
+  },
+  computed: {
+    layout() {
+      return (this.$route.meta.layout || default_layout) + "-layout";
+    }
+  },
+  data() {
+    return {
+      blah: "erik"
+    };
+  }
+};
+</script>  
+
+<style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
